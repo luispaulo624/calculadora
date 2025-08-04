77c66c7a324ed4ed749b8596c2f333f1ce45c259
Luís <luispaulogoncalvesteixeira1@gmail.com>
2025-08-04 15:01:44 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+# Lê dois valores numéricos do usuário
+num1 = float(input("Digite o primeiro valor: "))
+num2 = float(input("Digite o segundo valor: "))
+
+# Realiza as operações matemáticas
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+# Imprime os resultados na tela
+print(f"A soma de {num1} e {num2} é: {soma}")
+print(f"A subtração de {num1} e {num2} é: {subtracao}")
+print(f"A multiplicação de {num1} e {num2} é: {multiplicacao}")
+print(f"A divisão de {num1} por {num2} é: {divisao}")
